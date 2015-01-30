2f75c69e19fb5f5e35914ce38edd3b667a827a5a
Maximilian Hoffmann <account@maximilianhoffmann.com>
2015-01-30 16:23:40 +0100

    add index.html

--- .env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var defaults = require('./nodemon.json');
-
-module.exports = {
-  NODE_ENV: process.env.NODE_ENV || defaults.env.NODE_ENV,
-  API_URL: process.env.API_URL || defaults.env.API_URL,
-  APP_URL: process.env.APP_URL || defaults.env.APP_URL
-};
